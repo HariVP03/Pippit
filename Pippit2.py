539c95d3e22ad6818b09d12797130c87d563ca7c
Hari Vishnu Parashar <harryskotch11@gmail.com>
2019-10-23 19:09:38 +0530

    Final Commit

--- Pippit2.py
+++ Pippit2.py
@@ -1,18 +1,34 @@
 from bs4 import BeautifulSoup
 import urllib.request
 import tkinter as tk
+import tkinter.messagebox
+import io
+from matplotlib import pyplot as plt
 
 
 def remove_space(x):
 
     return '%20'.join(x.split())
 
+
 def remove_space_dash(x):
 
     return '-'.join(x.split())
 
 
-# Includes: Amazon(IN), Flipkart, Snapdeal, OLX, Ebay(IN), ShopClues, PaytmMall
+def get_int(s):
+    a = []
+    for i in range(len(s)):
+
+        if s[i].isdigit() or (s[i] == '.' and (s[i-1] != 's' and i != 0)):
+            a.append(s[i])
+    a = ''.join(a)
+
+    x = float(a)
+    return int(x)
+
+
+# Includes: Amazon(IN), Flipkart, Snapdeal, Ebay(IN), ShopClues, PaytmMall
 def amazon_price(item_name):
 
     item_name_spaced = remove_space(item_name)
@@ -34,6 +50,7 @@ def amazon_price(item_name):
         return ''.join(list(item_price))
     except:
         return 'Item Not Found... :('
+
 
 def amazon_pname(item_name):
 
@@ -244,6 +261,7 @@ def sclues_pname(item_name):
     except:
         return 'Item Not Found... :('
 
+
 def ptm_price(item_name):
 
     item_name_spaced = remove_space(item_name)
@@ -291,25 +309,212 @@ def ptm_pname(item_name):
     except:
         return 'Item Not Found... :('
 
+
+def find(l,s):
+
+    for i in range(len(l)):
+
+        if l[i]==s:
+            return i
+
+    else:
+        return False
+
+
+def show_stats_amzn(item_name):
+
+    try:
+        with open('Previous Searches.txt', 'r') as file:
+
+            text = file.read()
+            removed_symbs = ''.join(text).split()
+
+        item_tofind = remove_space_dash(item_name)
+        item_index = find(removed_symbs, item_tofind)
+
+        prv_amzn = removed_symbs[item_index+1]
+        new_amzn = amazon_price(item_name)
+
+        x = [0, 10]
+        y = [int(prv_amzn), get_int(new_amzn)]
+
+        plt.plot(x, y)
+        plt.show()
+    except:
+        tkinter.messagebox.showinfo('Error', 'No previous history of search found.')
+
+
+def show_stats_flpk(item_name):
+
+    try:
+        with open('Previous Searches.txt', 'r') as file:
+
+            text = file.read()
+            removed_symbs = ''.join(text).split()
+
+        item_tofind = remove_space_dash(item_name)
+        item_index = find(removed_symbs, item_tofind)
+
+        prv_flpk = removed_symbs[item_index+2]
+        new_flpk = flipkart_price(item_name)
+
+        x = [0, 10]
+        y = [int(prv_flpk), get_int(new_flpk)]
+
+        plt.plot(x, y)
+        plt.show()
+    except:
+        tkinter.messagebox.showinfo('Error', 'No previous history of search found.')
+
+
+def show_stats_sndp(item_name):
+
+    try:
+        with open('Previous Searches.txt', 'r') as file:
+
+            text = file.read()
+            removed_symbs = ''.join(text).split()
+
+        item_tofind = remove_space_dash(item_name)
+        item_index = find(removed_symbs, item_tofind)
+
+        prv_sndp = removed_symbs[item_index+3]
+        new_sndp = snapdeal_price(item_name)
+
+        x = [0, 10]
+        y = [int(prv_sndp), get_int(new_sndp)]
+
+        plt.plot(x, y)
+        plt.show()
+    except:
+        tkinter.messagebox.showinfo('Error', 'No previous history of search found.')
+
+
+def show_stats_ebay(item_name):
+
+    try:
+        with open('Previous Searches.txt', 'r') as file:
+
+            text = file.read()
+            removed_symbs = ''.join(text).split()
+
+        item_tofind = remove_space_dash(item_name)
+        item_index = find(removed_symbs, item_tofind)
+
+        prv_ebay = removed_symbs[item_index+4]
+        new_ebay = ebayin_price(item_name)
+
+        x = [0, 10]
+        y = [int(prv_ebay), get_int(new_ebay)]
+
+        plt.plot(x, y)
+        plt.show()
+    except:
+        tkinter.messagebox.showinfo('Error', 'No previous history of search found.')
+
+
+def show_stats_sclues(item_name):
+
+    try:
+        with open('Previous Searches.txt', 'r') as file:
+
+            text = file.read()
+            removed_symbs = ''.join(text).split()
+
+        item_tofind = remove_space_dash(item_name)
+        item_index = find(removed_symbs, item_tofind)
+
+        prv_sclues = removed_symbs[item_index+5]
+        new_sclues = sclues_price(item_name)
+
+        x = [0, 10]
+        y = [int(prv_sclues), get_int(new_sclues)]
+
+        plt.plot(x, y)
+        plt.show()
+    except:
+        tkinter.messagebox.showinfo('Error', 'No previous history of search found.')
+
+
+def show_stats_ptm(item_name):
+
+    try:
+        with open('Previous Searches.txt', 'r') as file:
+
+            text = file.read()
+            removed_symbs = ''.join(text).split()
+
+        item_tofind = remove_space_dash(item_name)
+        item_index = find(removed_symbs, item_tofind)
+
+        prv_ptm = removed_symbs[item_index+6]
+        new_ptm = ptm_price(item_name)
+
+        x = [0, 10]
+        y = [int(prv_ptm), get_int(new_ptm)]
+
+        plt.plot(x, y)
+        plt.show()
+    except:
+        tkinter.messagebox.showinfo('Error', 'No previous history of search found.')
+
+
+def previous_searches():
+
+    with open('Previous Searches.txt', 'r') as file:
+        text = file.read()
+        removed_symbs = ''.join(text).split()
+        removed_symbs = '\t'.join(removed_symbs)
+
+    label_hist = tk.Label(text=removed_symbs)
+    label_hist.place(relheight=1, relwidth=1, relx=0, rely=0)
+
+    back = tk.Button(label_hist, text='Back', command= lambda: label_hist.destroy())
+    back.place(relheight=0.05, relwidth=0.05, relx=0, rely=0)
+
+
 def compld(item_name):
 
     label_amazon_name['text'] = amazon_pname(item_name)
-    label_amazon_price['text'] = amazon_price(item_name)
+    amzn_price = label_amazon_price['text'] = amazon_price(item_name)
 
     label_flipkart_name['text'] = flipkart_pname(item_name)
-    label_flipkart_price['text'] = flipkart_price(item_name)
+    flpk_price = label_flipkart_price['text'] = flipkart_price(item_name)
 
     label_snapdeal_name['text'] = snapdeal_pname(item_name)
-    label_snapdeal_price['text'] = snapdeal_price(item_name)
+    snd_price = label_snapdeal_price['text'] = snapdeal_price(item_name)
 
     label_ebay_name['text'] = ebayin_pname(item_name)
-    label_ebay_price['text'] = ebayin_price(item_name)
+    ebay_price = label_ebay_price['text'] = ebayin_price(item_name)
 
     label_sclues_name['text'] = sclues_pname(item_name)
-    label_sclues_price['text'] = sclues_price(item_name)
+    scl_price = label_sclues_price['text'] = sclues_price(item_name)
 
     label_ptm_name['text'] = ptm_pname(item_name)
-    label_ptm_price['text'] = ptm_price(item_name)
+    paytm_price = label_ptm_price['text'] = ptm_price(item_name)
+
+    # Storing Search
+
+    with io.open('Previous Searches.txt', 'a', encoding='utf-8') as file:
+
+        file.write('\n')
+        file.write(remove_space_dash(item_name))
+        file.write(f" \n{get_int(amzn_price)} \t")
+        file.write(f" \n{get_int(flpk_price)} \t")
+        file.write(f" \n{get_int(snd_price)} \t")
+        file.write(f" \n{get_int(ebay_price)} \t")
+        file.write(f" \n{get_int(scl_price)} \t")
+        file.write(f" \n{get_int(paytm_price)} \t")
+        file.write('\n')
+
+        print(amzn_price)
+        print(flpk_price)
+        print(snd_price)
+        print(ebay_price)
+        print(scl_price)
+        print(paytm_price)
+
+
 
 # Graphical User Interface:
 
@@ -323,14 +528,21 @@ canvas = tk.Canvas()
 canvas.place(relheight=1, relwidth=1)
 
 # Setting
-
 menu = tk.Menu(root)
 
 sub = tk.Menu(menu, tearoff=0)
 
 menu.add_cascade(label='Settings', menu=sub)
-sub.add_command(label='Statistics')
+sub.add_command(label='Statistics of Previous Prices on Amazon', command=(lambda: show_stats_amzn(search_entry.get())))
+sub.add_command(label='Statistics of Previous Prices on Flipkart', command=(lambda: show_stats_flpk(search_entry.get())))
+sub.add_command(label='Statistics of Previous Prices on Snapdeal', command=(lambda: show_stats_sndp(search_entry.get())))
+sub.add_command(label='Statistics of Previous Prices on Ebay', command=(lambda: show_stats_ebay(search_entry.get())))
+sub.add_command(label='Statistics of Previous Prices on ShopClues', command=(lambda: show_stats_sclues(search_entry.get())))
+sub.add_command(label='Statistics of Previous Prices on PayTM', command=(lambda: show_stats_ptm(search_entry.get())))
 
+sub2 = tk.Menu(menu, tearoff=0)
+menu.add_cascade(label='Others', menu=sub2)
+sub2.add_command(label='Previous Searches', command=previous_searches)
 root.config(menu=menu)
 
 # Amazon
